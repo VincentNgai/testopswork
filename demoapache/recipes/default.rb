05e239f283d208e0a6f646504992f8020ac5733b
Vincent Ngai <vincentngai@jobsdb.com>
2016-02-18 12:17:28 +0800

    no message

--- demoapache/recipes/default.rb
+++ demoapache/recipes/default.rb
@@ -1,18 +1,4 @@
-execute "update-upgrade install ntop" do
+execute "update-upgrade" do
   command "apt-get update && apt-get install git ntop"
   action :run
 end
-
-package 'apache2' do
-  action :install
-end
-
-service 'apache2' do
-  action [ :enable, :start ]
-end
-
-cookbook_file '/var/www/index.html' do
-  source 'index.html'
-  mode '0644'
-end
-
